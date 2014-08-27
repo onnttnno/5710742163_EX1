ccd26edc2f7291bfc2dc01cc9bb2e14daaf2dfbd
onnttnno <sanamori@msn.com>
2014-08-27 17:11:31 +0700

    ข้อ1.3

---
+++ 5710742163Ex1.1/EX1.3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a ;
+    int b ;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    //a*b;//
+    //a/b;//
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    return 0;
+}
