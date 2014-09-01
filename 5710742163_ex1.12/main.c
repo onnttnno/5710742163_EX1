45c9c1c52b58277867d9de569ccc3f3f0c4c1dbf
onnttnno <sanamori@msn.com>
2014-09-01 10:11:13 +0700

    5710742163_ex1.12

---
+++ 5710742163_ex1.12/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A;
+    float B;
+    printf("Please enter salary :  ");
+    scanf("%d",&A);
+    B=A*0.1;
+    printf("tax: %.2f",B);
+    return 0;
+}
