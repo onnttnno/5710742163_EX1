1232496e0c5ec974eae5cc1cfe0d9fc2a55e29ec
onnttnno <sanamori@msn.com>
2014-09-01 10:10:24 +0700

    5710742163_ex1.10

---
+++ 5710742163_ex1.10/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a;
+    printf("Please put in number: ");
+    scanf("%f",&a);
+    printf("Put out number: %.3f",a);
+    return 0;
+}
