79b819f9b7e4175f66bdf992d4444b0d4da8fd2f
onnttnno <sanamori@msn.com>
2014-09-01 10:12:21 +0700

    5710742163_ex1.2

---
+++ 5710742163_ex1.2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A;
+    int B;
+    int C;
+    printf(">Ener number: ");
+    scanf("%d %d %d",&A,&B,&C);
+    printf(">: %d\n",A);
+    printf(">: %d\n",B);
+    printf(">: %d",C);
+
+    return 0;
+}
