67d783dbcaa31568535adcf9c9860c3c4d0592d5
onnttnno <sanamori@msn.com>
2014-09-01 10:13:17 +0700

    5710742163_ex1.4

---
+++ 5710742163_ex1.4/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float principal;
+    float rate;
+    float total;
+    float interest;
+    float days ;
+    printf("Enter loan principal(-1to end):");
+    scanf("%f",&principal);
+    printf("Enter interest rate : ");
+    scanf("%f",&rate);
+    printf("Enter term of the loan in days : ");
+    scanf("%f",&days);
+    total=(days/365);
+    interest=(principal*rate*total);
+    printf("Total is : %f\n",total);
+    printf("The interest charge is: %.2f ",interest);
+    return 0;
+}
