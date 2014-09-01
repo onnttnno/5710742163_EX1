053358951f5c5891a93f96512a9b148244d6612c
onnttnno <sanamori@msn.com>
2014-09-01 10:10:50 +0700

    5710742163_ex1.11

---
+++ 5710742163_ex1.11/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A;
+    printf("Please Enter Denary  number: ");
+    scanf("%d",&A);
+    printf(" Hexadenary number is : %x\n",A);
+    printf(" Octanary number is : %o",A);
+    return 0;
+}
